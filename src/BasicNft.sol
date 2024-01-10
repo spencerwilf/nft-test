03ddbecb8cb32e470c34f6f4bdd86dc892e780ca
Spencer Wilfahrt <spencerwilf@yahoo.com>
2024-01-10 10:13:38 -0500

    uri

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -6,13 +6,15 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract BasicNft is ERC721 {
 
     uint private s_tokenCounter;
+    mapping(uint => string) private s_tokenIdToUri;
 
     constructor() ERC721("The Dawgs", "DOG") {
         s_tokenCounter = 0;
     }
 
-    function mintNft() public {
-
+    function mintNft(string memory tokenUri) public {
+        s_tokenIdToUri[s_tokenCounter] = tokenUri;
+        s_tokenCounter++;
     }
 
     function tokenURI(uint tokenId) public view override returns(string memory) {
