64798643c3e1174c848d25b14627c8ae0ef56782
Spencer Wilfahrt <spencerwilf@yahoo.com>
2024-01-10 12:38:36 -0500

    uri

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -2,12 +2,20 @@
 pragma solidity 0.8.21;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MoodNft is ERC721 {
     uint256 private s_tokenCounter;
     mapping(uint256 => string) private s_tokenIdToUri;
     string private sadSvgImageUri;
     string private happySvgImageUri;
+
+    enum Mood {
+        HAPPY,
+        SAD
+    }
+
+    mapping(uint256 => Mood) private s_tokenIdToMood;
 
     constructor(string memory _sadSvg, string memory _happySvg) ERC721("Mood NFT", "MN") {
         s_tokenCounter = 0;
@@ -17,10 +25,39 @@ contract MoodNft is ERC721 {
 
     function mintNft() public {
         _safeMint(msg.sender, s_tokenCounter);
+        s_tokenIdToMood[s_tokenCounter] = Mood.HAPPY;
         s_tokenCounter++;
     }
 
-    function tokenURI(uint tokenId) public view override returns(string memory) {
+    function _baseURI() internal pure override returns(string memory) {
+        return "data:application/json;base64,";
+    }
 
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        string memory imageURI;
+
+        if (s_tokenIdToMood[tokenId] == Mood.HAPPY) {
+            imageURI = happySvgImageUri;
+        } else {
+            imageURI = sadSvgImageUri;
+        }
+
+        return string(
+            abi.encodePacked(
+                _baseURI(),
+                Base64.encode(
+                    bytes( // bytes casting actually unnecessary as 'abi.encodePacked()' returns a bytes
+                        abi.encodePacked(
+                            '{"name":"',
+                            name(), // You can add whatever name here
+                            '", "description":"An NFT that reflects the mood of the owner, 100% on Chain!", ',
+                            '"attributes": [{"trait_type": "moodiness", "value": 100}], "image":"',
+                            imageURI,
+                            '"}'
+                        )
+                    )
+                )
+            )
+        );
     }
 }
