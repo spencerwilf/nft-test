955046bdd3c1aa20ab95a5c2b7e68bad39209aa2
Spencer Wilfahrt <spencerwilf@yahoo.com>
2024-01-10 12:11:59 -0500

    uris

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -6,12 +6,21 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract MoodNft is ERC721 {
     uint256 private s_tokenCounter;
     mapping(uint256 => string) private s_tokenIdToUri;
-    string private sadSvg;
-    string private happySvg;
+    string private sadSvgImageUri;
+    string private happySvgImageUri;
 
     constructor(string memory _sadSvg, string memory _happySvg) ERC721("Mood NFT", "MN") {
         s_tokenCounter = 0;
-        happySvg = _happySvg;
-        sadSvg = _sadSvg;
+        happySvgImageUri = _happySvg;
+        sadSvgImageUri = _sadSvg;
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
+
+    function tokenURI(uint tokenId) public view override returns(string memory) {
+
     }
 }
