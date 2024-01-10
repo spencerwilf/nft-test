6b014a6356bc947ca1d4998108cf576a54e85978
Spencer Wilfahrt <spencerwilf@yahoo.com>
2024-01-10 10:26:56 -0500

    tests start

---
+++ test/BasicNftTest.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.21;
+
+import {Test} from "forge-std/Test.sol";
+import {BasicNft} from "../src/BasicNft.sol";
+import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
+
+contract BasicNftTest is Test {
+    DeployBasicNft public deployer;
+    BasicNft public basicNft;
+
+    function setUp() public {
+        
+    }
+}
