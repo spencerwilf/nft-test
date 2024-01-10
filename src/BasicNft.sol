7b91031651785f589ff891353b95bcea0166975f
Spencer Wilfahrt <spencerwilf@yahoo.com>
2024-01-09 22:07:46 -0500

    constructor

---
+++ src/BasicNft.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNft is ERC721 {
+    constructor() ERC721("The Dawgs", "DOG") {
+
+    }
+}
