d848be74c5e4342c2b12f7e086c9a5b0274c846b
Spencer Wilfahrt <spencerwilf@yahoo.com>
2024-01-10 11:13:13 -0500

    mint test

--- test/BasicNftTest.t.sol
+++ test/BasicNftTest.t.sol
@@ -8,6 +8,8 @@ import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
 contract BasicNftTest is Test {
     DeployBasicNft public deployer;
     BasicNft public basicNft;
+    address public USER = makeAddr("user");
+    string constant PUG = "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
     function setUp() public {
         deployer = new DeployBasicNft();
@@ -16,5 +18,13 @@ contract BasicNftTest is Test {
 
     function testNameIsCorrect() public {
         assertEq(keccak256(abi.encodePacked(basicNft.name())), keccak256(abi.encodePacked("The Dawgs")));
+    }
+
+    function testCanMintAndHaveBalance() public {
+        vm.prank(USER);
+        basicNft.mintNft(PUG);
+
+        assert(basicNft.balanceOf(USER) == 1);
+        assert(keccak256(abi.encodePacked(PUG)) == keccak256(abi.encodePacked(basicNft.tokenURI(0))));
     }
 }
